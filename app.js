d13f24047d14d3c405c3d68d2fabe96d04e69e2d
Hannan-Shahzad <yyo85281@gmail.com>
2024-11-24 18:38:58 -0800

    Updated chat app project with latest changes

--- app.js
+++ app.js
@@ -1,51 +1,110 @@
-const express= require('express')
-const app =express();
-const PORT=4000|| process.env.PORT;
-const path=require('path');
+// //app.js
 
-const server = app.listen(PORT,()=>{
-console.log("program running on port ",PORT);
+// const express= require('express')
+// const app =express();
+// const PORT=4000|| process.env.PORT;
+// const path=require('path');
+
+// const server = app.listen(PORT,()=>{
+// console.log("program running on port ",PORT);
+// });
+
+// app.use(express.static(path.join(__dirname,'public')))
+
+// const io=require('socket.io')(server);
+
+// let socketsConnected=new Set();
+
+// io.on('connection',onConnected);
+
+// function onConnected(socket){
+//     console.log(socket.id);
+//     socketsConnected.add(socket.id);
+
+//     io.emit('client-total',socketsConnected.size);
+
+//     socket.on('disconnect',()=>{
+
+//         console.log('disconnected')
+//         socketsConnected.delete(socket.id);
+//         io.emit('client-total',socketsConnected.size);
+
+//     });
+
+
+// //      <li class="message-feedback">
+// //     <p class="feedback" id="feedback">
+// //         killer is typing
+// //     </p>
+// // </li> 
+//     socket.on('message',(data)=>{
+//         console.log(data);
+//         socket.broadcast.emit('chat-message',data)
+//     })
+
+//     socket.on('feedback',(data)=>{
+
+//         socket.broadcast.emit('feedback',data)
+
+//     })
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+const server = app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-const io=require('socket.io')(server);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*', // Allow cross-origin requests
+        methods: ['GET', 'POST'],
+    },
+});
 
-let socketsConnected=new Set();
+let socketsConnected = new Set();
 
-io.on('connection',onConnected);
-
-function onConnected(socket){
-    console.log(socket.id);
+io.on('connection', (socket) => {
+    console.log(`New connection: ${socket.id}`);
     socketsConnected.add(socket.id);
 
-    io.emit('client-total',socketsConnected.size);
+    io.emit('client-total', socketsConnected.size);
 
-    socket.on('disconnect',()=>{
-
-        console.log('disconnected')
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
         socketsConnected.delete(socket.id);
-        io.emit('client-total',socketsConnected.size);
-
+        io.emit('client-total', socketsConnected.size);
     });
 
-
-//      <li class="message-feedback">
-//     <p class="feedback" id="feedback">
-//         killer is typing
-//     </p>
-// </li> 
-    socket.on('message',(data)=>{
+    socket.on('message', (data) => {
         console.log(data);
-        socket.broadcast.emit('chat-message',data)
-    })
+        socket.broadcast.emit('chat-message', data);
+    });
 
-    socket.on('feedback',(data)=>{
-
-        socket.broadcast.emit('feedback',data)
-
-    })
-}
-
-
-
+    socket.on('feedback', (data) => {
+        socket.broadcast.emit('feedback', data);
+    });
+});
